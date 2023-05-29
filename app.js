92a329de9b65ecb5a64aa1a0f74c129137616efc
tareqh86 <tareqhabbyullah@gmail.com>
2023-05-29 17:37:19 +0700

    nambah middleware

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const morgan = require('morgan');
+
 // Gunakan EJS
 app.set('view engine', 'ejs');
 
+// Application Level Middleware
+// app.use((req, res, next) => {
+//   console.log('Time:', Date.now());
+//   next();
+// });
+
+// Built-In middleware
+app.use(express.static('public'));
+
+// Third Party Middleware
+// app.use(morgan('common'));
+
 app.get('/', (req, res) => {
-  res.render('index', { nama: 'Lebah ganteng' });
+  res.render('index', {
+    tittle: 'Home',
+    nama: 'Lebah ganteng',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.render('about', {
+    tittle: 'About',
+  });
 });
 
 app.get('/contact', (req, res) => {
@@ -32,7 +51,10 @@ app.get('/contact', (req, res) => {
     },
   ];
 
-  res.render('contact', { contact });
+  res.render('contact', {
+    tittle: 'Contact',
+    contact,
+  });
 });
 
 // Get by Query
@@ -44,6 +66,11 @@ app.get('/users/:userId', (req, res) => {
 app.get('/users/:userId/books/:bookId', (req, res) => {
   // res.send(req.params);
   res.send(`ID User : ${req.params.userId} <br> ID Book : ${req.params.bookId} `);
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('<h1>404 Not Found</h1>');
 });
 
 app.listen(port, () => {
